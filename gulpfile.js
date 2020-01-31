2876f618c07349b0c7a7bea982dbd7c9086c75a5
ebremner <ebremner@uwaterloo.ca>
2020-01-31 11:13:56 -0500

    Updating packages and gulpfile to use latest versions

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,49 @@
 var pkg = require('./package.json'),
   gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  minifyCSS = require('gulp-minify-css'),
-  plugins = require('gulp-load-plugins')(),
-  browserSync = require('browser-sync').create();
+  glob = require('glob'),
+  minifyCSS = require('gulp-clean-css'),
+  plugins = require('gulp-load-plugins')();
+
 
 var config = {
-  sass:'./sass/*.{scss,sass}',
-  sassSrc:'./sass/griffin_brothers.scss',
-  sassIe:'./sass/ie.scss',
-  sassPrint:'./sass/print.scss',
-  css:'./css',
+  sass: './sass/**/*.{scss,sass}',
+  sassSrc: './sass/griffin_brothers.scss',
+  css: './css',
   js:'./scripts',
-  jsSrc:'./js/resp.js',
-  images:'./images/*.{png,gif,jpeg,jpg,svg}',
-  imagesmin:'./images/minified',
+  jsSrc:'./js/griffin_brothers.js'
 };
 
-var AUTOPREFIXER_BROWSERS = [
-  '> 1%',
-  'ie >= 8',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4',
-  'bb >= 10'
-];
+// Transpile, concatenate and minify scripts
+function scripts() {
+  return gulp.src(config.jsSrc)
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'))
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest(config.js));
+}
 
-gulp.task('styles',function () {
+// Compile styles.
+function styles() {
   return gulp.src(config.sassSrc)
-  // .pipe(plugins.sourcemaps.init())
     .pipe(plugins.plumber())
     .pipe(plugins.sass({
       includePaths: require('node-bourbon').includePaths,
       outputStyle: 'collapsed'
     }))
-    .on('error', function (err) {
-      gutil.log(err);
-      gutil.beep();
-      this.emit('end');
-    })
-    .pipe(plugins.autoprefixer({
-      browsers:AUTOPREFIXER_BROWSERS,
-      cascade: false
-    }))
     .pipe(minifyCSS())
-    .pipe(plugins.concat('griffin_brothers.css'))
+    .pipe(plugins.concat('rwd.css'))
     .pipe(gulp.dest(config.css))
-    .pipe(browserSync.stream())
     .pipe(plugins.size({title:'css'}));
-});
+}
 
-gulp.task('watch',[],function () {
-  gulp.watch(config.sass,['styles']);
-});
+// Watch files.
+function watchFiles() {
+  gulp.watch(config.sass, styles);
+  gulp.watch(config.jsSrc, scripts);
+}
 
-gulp.task('default',[],function () {
-  gulp.start('styles','watch');
-});
+const build = gulp.series(styles, scripts, watchFiles);
+const watch = gulp.series(watchFiles);
+
+exports.watch = watch;
+exports.default = build;
